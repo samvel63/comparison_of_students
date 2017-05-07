9a17f81521de2cd73fb813eee5e2019fc6f4faf5
samvel63 <mhitaryan.1998@mail.ru>
2017-05-07 10:29:27 +0300

    Добавлены тесты на больших данных

---
+++ gen_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from random import *
+from string import ascii_letters
+
+template = '{surname} {initials}\t{class}\n'
+
+with open('input2', 'w') as f:
+	for i in range(1, 10 * 1000 * 1000):
+		if i % 10000 == 0:
+			print i
+		f.write(template.format(**{
+			'surname': ''.join(choice(ascii_letters) for _ in range(randint(1, 63 + 1))),
+			'initials': choice(ascii_letters) + choice(ascii_letters),
+			'class': str(randint(1, 11))
+		}))
